7278ac319469573199c19ade5f1cbcf3479f757b
Billy Hickman <me@billyhickman.com>
2013-03-12 16:16:50 +0000

    Functioning heat map added NEEDS STYLING

--- application/views/components/header.blade.php
+++ application/views/components/header.blade.php
@@ -7,15 +7,14 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
 	<script src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAisD4YF47Qpu_VBhq9wUb6lbIAH61816Y&sensor=false"></script>
-	<script src = "http://www.heatmapapi.com/Javascript/HeatmapAPIGoogle3.js"></script>
-	<script src = "http://www.heatmapapi.com/Javascript/HeatMapAPI3.aspx?k=6e70c658-52cf-4224-8403-4440943e53f4"></script>
 	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
 	{{ HTML::script('http://d3js.org/d3.v3.min.js') }}
 	{{ HTML::style('css/bootstrap.css') }}
 	{{ HTML::style('css/main.css') }}
 	{{ HTML::style('css/font-awesome.css') }}
 	{{ HTML::style('css/bootstrap-responsive.css') }}
+	{{ HTML::script('js/heatmap-gmaps.js')}}
+	{{ HTML::script('js/heatmap.js')}}
 	{{ HTML::script('js/d3.layout.cloud.js')}}
 	{{ HTML::script('js/main.js')}}
-	
 </head>
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -475,64 +475,63 @@ function heatMap(filename, div) {
 	var renderAt = makeId(div);
 	var div = div; 
 
-	$.getJSON("http://localhost/visucosmos-git/public_html/"+file, function(data) {
+    var myLatlng = new google.maps.LatLng(51.517, 0.1062);
+    // define map properties
+    var myOptions = {
+      zoom: 8,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: false,
+      scrollwheel: true,
+      draggable: true,
+      navigationControl: true,
+      mapTypeControl: false,
+      scaleControl: true,
+      disableDoubleClickZoom: false
+    };
+    // we'll use the heatmapArea 
+    var map = new google.maps.Map($(renderAt)[0], myOptions);
+    
+    // let's create a heatmap-overlay
+    // with heatmap config properties
+    var heatmap = new HeatmapOverlay(map, {
+        "radius":20,
+        "visible":true, 
+        "opacity":60
+    });
+ 
+    // here is our dataset
+    // important: a datapoint now contains lat, lng and count property!
+    
+    var testData = getData(file); 
+
+ 
+    // now we can set the data
+    google.maps.event.addListenerOnce(map, "idle", function(){
+        // this is important, because if you set the data set too early, the latlng/pixel projection doesn't work
+        heatmap.setDataSet(testData);
+    });
+ 
+ 	function getData(file)
+ 	{
+ 		var topJsonArray = {
+			max: 46,
+			data: []};
+
+		$.getJSON("http://localhost/visucosmos-git/public_html/"+file, function(data) {
 		// $.getJSON("http://visucosmos.info/"+file, function(data) {
-		var myData = new Array(); 
-		var lat = new Array(); 
-		var lon = new Array();
-		var weight = new Array();
-
-		$.each(data, function(key, val) {
-			// $.each(val, function(key, val) {
-				$.each(val, function(key, d){
-					if(val['attr7'] != "-" && val['attr6'] != "-")
-					{
-						lat.push(val['attr6']);
-						lon.push(val['attr7']);
-						weight.push(1);
-					}	
-				});
-			// });
-		});
-		createVis(lat, lon, weight, div);
-	});
-
-	function createVis(lat, lon, weight, div) 
-	{
-		var myHeatmap = new GEOHeatmap();
-		var myData = null;
-		$(function() {
-			// create data
-			myData = new Array();
-			for (p = 0; p < 50; p++) {
-			 var rLatD = lat[p];
-			 var rLonD = lon[p];
-			 var rValD = weight[p];
-
-			 myData.push(38.47 + (rLatD / 15000));
-			 myData.push(-121.84 + (rLonD / 15000));
-			 myData.push(rValD);
-			}
-
-			// configure HeatMapAPI
-			myHeatmap.Init(241, 208); // set at pixels for your map
-			myHeatmap.SetBoost(0.8);
-			myHeatmap.SetDecay(0); // see documentation
-			myHeatmap.SetData(myData);
-			myHeatmap.SetProxyURL('http://www.yourwebsite.com/proxy.php');
-
-			// set up Google map, pass in the heatmap function
-			var myLatlng = new google.maps.LatLng(51.5171, 0.1062);
-			var myOptions = {
-			 zoom: 11,
-			 center: myLatlng,
-			 mapTypeId: google.maps.MapTypeId.ROADMAP
-			}
-			var map = new google.maps.Map(document.getElementById(div), myOptions);
-			google.maps.event.addListener(map, 'idle', function(event) {
-			 myHeatmap.AddOverlay(this, myHeatmap);
+			$.each(data, function(key, val) {
+				// $.each(val, function(key, val) {
+				if(val['attr6'] != "-"){
+					topJsonArray.data.push({lat : val['attr6'], lng: val['attr7'], count: 46});
+				}	
+				// });
 			});
-		});
+ 		});
 
-	}
+ 		console.log(topJsonArray);
+ 		return topJsonArray; 
+
+ 		
+ 	}
 }
